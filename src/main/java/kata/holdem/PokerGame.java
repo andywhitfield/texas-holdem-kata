82c1768ceb5cc9a21791ad193c8f92eec53050d8
Andy Whitfield <andy@andywhitfield.me.uk>
2010-07-06 18:39:00 +0100

    Added map to store cards dealt to players.

--- src/main/java/kata/holdem/PokerGame.java
+++ src/main/java/kata/holdem/PokerGame.java
@@ -1,7 +1,18 @@
 package kata.holdem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PokerGame {
-	public PokerGame deal(String player) { return this; }
+	private Map<String, List<String>> playersAndTheirCards = new HashMap<String, List<String>>();
+	
+	public PokerGame deal(String player) {
+		playersAndTheirCards.put(player, new ArrayList<String>());
+		return this;
+	}
+	
 	public PokerGame holeCards(String card1, String card2) { return this; }
 	public PokerGame dealFlop(String card, String card2, String card3) { return this; }
 	public PokerGame dealTurn(String card1) { return this; }
