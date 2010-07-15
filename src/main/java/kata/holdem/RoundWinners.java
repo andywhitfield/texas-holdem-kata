4f568f9385ac155dac1e43fc3ea9acef5849448c
Andy Whitfield <andy@andywhitfield.me.uk>
2010-07-15 18:54:16 +0100

    Made a note in the class, and also the issues, that we need to introduce a Card class.

--- src/main/java/kata/holdem/RoundWinners.java
+++ src/main/java/kata/holdem/RoundWinners.java
@@ -9,6 +9,10 @@ public class RoundWinners {
 	
 	public RoundWinners(Map<String, List<String>> playersAndTheirCards) {
 		this.playersAndTheirCards = playersAndTheirCards;
+		// TODO: what we want to be able to do is go through all the players
+		// and identify their top card. Then we can determine the winner.
+		// But all we have is strings, so we now we should probably
+		// introduce a Card class.
 	}
 
 	public boolean isWinner(String player) {
