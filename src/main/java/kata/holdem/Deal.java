1196872b74b536beb431599323aa4509a3b92c71
Andy Whitfield <andy@andywhitfield.me.uk>
2010-10-05 18:13:33 +0100

    Added the concept of a Deal (as before, the new code is still unused).

---
+++ src/main/java/kata/holdem/Deal.java
@@ -0,0 +1,19 @@
+package kata.holdem;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Deal {
+	private final List<Card> cardsInDeal;
+	private final Set<String> playersFolded = new HashSet<String>();
+
+	public Deal(List<Card> cards) {
+		this.cardsInDeal = cards;
+	}
+	
+	public List<Card> getCardsInDeal() { return this.cardsInDeal; }
+	public void fold(String player) { this.playersFolded.add(player); }
+	public boolean folded(String player) { return this.playersFolded.contains(player); }
+	public Set<String> getPlayersFolded() { return this.playersFolded; }
+}
--- src/main/java/kata/holdem/PokerRound.java
+++ src/main/java/kata/holdem/PokerRound.java
@@ -9,6 +9,7 @@ import java.util.Set;
 
 public class PokerRound {
 	private Map<String, HoleCards> playerInfo = new HashMap<String, HoleCards>();
+	private List<Deal> deals = new ArrayList<Deal>();
 	
 	private List<String> players = new ArrayList<String>();
 	private Set<String> foldedPlayers = new HashSet<String>();
@@ -25,24 +26,28 @@ public class PokerRound {
 	}
 		
 	public PokerRound dealFlop(String card1, String card2, String card3) {
+		deals.add(new Deal(Cards.from(card1, card2, card3)));
 		for (List<Card> cards : playersAndTheirCards.values())
 			cards.addAll(Cards.from(card1, card2, card3));
 		return this;
 	}
 	
 	public PokerRound dealTurn(String card) {
+		deals.add(new Deal(Cards.from(card)));
 		for (List<Card> cards : playersAndTheirCards.values())
 			cards.add(Card.from(card));
 		return this;
 	}
 	
 	public PokerRound dealRiver(String card) {
+		deals.add(new Deal(Cards.from(card)));
 		for (List<Card> cards : playersAndTheirCards.values())
 			cards.add(Card.from(card));
 		return this;
 	}
 
 	public PokerRound fold(String player) {
+		deals.get(deals.size() - 1).fold(player);
 		foldedPlayers.add(player);
 		return this;
 	}
