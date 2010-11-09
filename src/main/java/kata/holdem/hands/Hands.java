091c50dccca7715776a52c64bd8bee6fe9d9efba
Andy Whitfield <andy@andywhitfield.me.uk>
2010-11-09 18:17:13 +0000

    Refactored duplication into a common Hands class.

---
+++ src/main/java/kata/holdem/hands/Hands.java
@@ -0,0 +1,23 @@
+package kata.holdem.hands;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import kata.holdem.Card;
+import kata.holdem.collections.Iterables;
+
+public class Hands {
+	public static List<Map.Entry<Integer, Collection<Card>>> existSameValuedCards(Iterable<Card> cards, int numberOfSameValuedCards) {
+		Map<Integer, Collection<Card>> groupedByValue = Iterables.groupBy(cards, new CardNumericValue());
+		Collection<Map.Entry<Integer, Collection<Card>>> sameValuedCards = Iterables.where(groupedByValue.entrySet(), new ContainsSameValuedCards(numberOfSameValuedCards));
+		
+		return Iterables.sort(sameValuedCards, new Comparator<Map.Entry<Integer, Collection<Card>>>() {
+			@Override
+			public int compare(Entry<Integer, Collection<Card>> o1, Entry<Integer, Collection<Card>> o2) {
+				return o2.getKey().compareTo(o1.getKey());
+			}});
+	}
+}
--- src/main/java/kata/holdem/hands/PairIdentifier.java
+++ src/main/java/kata/holdem/hands/PairIdentifier.java
@@ -2,22 +2,19 @@ package kata.holdem.hands;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import kata.holdem.Card;
 import kata.holdem.RankedHand;
-import kata.holdem.collections.Iterables;
 
 public class PairIdentifier implements HandIdentifier {
 	@Override
 	public RankedHand accept(String player, Iterable<Card> cards) {
-		// group cards by card value...
-		Map<Integer, Collection<Card>> groupedByValue = Iterables.groupBy(cards, new CardNumericValue());
-		Collection<Map.Entry<Integer, Collection<Card>>> pairs = Iterables.where(groupedByValue.entrySet(), new ContainsSameValuedCards(2));
-		
+		List<Map.Entry<Integer, Collection<Card>>> pairs = Hands.existSameValuedCards(cards, 2);
 		if (pairs.size() != 1) return null;
 		
 		// there's a pair
-		return new RankedHand(player, cards, this, new ArrayList<Card>(pairs.iterator().next().getValue()));
+		return new RankedHand(player, cards, this, new ArrayList<Card>(pairs.get(0).getValue()));
 	}
 }
