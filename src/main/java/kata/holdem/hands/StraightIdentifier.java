a1b1e0b2d4f08eb946e5169f0bd249dec77b7eaa
Andy Whitfield <andy@andywhitfield.me.uk>
2010-11-16 18:35:43 +0000

    First implementation of straight hand identifier.

--- src/main/java/kata/holdem/hands/StraightIdentifier.java
+++ src/main/java/kata/holdem/hands/StraightIdentifier.java
@@ -1,11 +1,34 @@
 package kata.holdem.hands;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import kata.holdem.Card;
 import kata.holdem.RankedHand;
+import kata.holdem.collections.Action;
+import kata.holdem.collections.Iterables;
 
 public class StraightIdentifier implements HandIdentifier {
 	@Override
 	public RankedHand accept(String player, Iterable<Card> cards) {
+		List<Map.Entry<Integer, Collection<Card>>> inOrder = Hands.existSameValuedCards(cards, 1);
+		while (inOrder.size() >= 5) {
+			if (inOrder.get(0).getKey() - 4 == inOrder.get(4).getKey())
+				return new RankedHand(player, cards, this, cardsFrom(inOrder));
+			inOrder.remove(0);
+		}
+		
 		return null;
+	}
+	
+	private List<Card> cardsFrom(List<Map.Entry<Integer, Collection<Card>>> firstFiveMakeTheStraight) {
+		return new ArrayList<Card>(Iterables.select(firstFiveMakeTheStraight.subList(0, 5), new Action<Map.Entry<Integer, Collection<Card>>, Card>() {
+			@Override
+			public Card action(Entry<Integer, Collection<Card>> cards) {
+				return cards.getValue().iterator().next();
+			}}));
 	}
 }
