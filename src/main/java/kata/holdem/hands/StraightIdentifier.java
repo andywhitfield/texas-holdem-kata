54a967254208dbc10edbf03db40bb56ae3c703de
Andy Whitfield <andy@andywhitfield.me.uk>
2010-11-17 18:21:58 +0000

    Added the implementation of aceLow which gets the test passing.

--- src/main/java/kata/holdem/hands/StraightIdentifier.java
+++ src/main/java/kata/holdem/hands/StraightIdentifier.java
@@ -10,8 +10,11 @@ import kata.holdem.Card;
 import kata.holdem.RankedHand;
 import kata.holdem.collections.Action;
 import kata.holdem.collections.Iterables;
+import kata.holdem.collections.Predicate;
 
 public class StraightIdentifier implements HandIdentifier {
+	private static final int aceValue = Card.from("Ah").getNumericValue();
+
 	@Override
 	public RankedHand accept(String player, Iterable<Card> cards) {
 		List<Map.Entry<Integer, Collection<Card>>> inOrder = Hands.existSameValuedCards(cards, 1);
@@ -21,15 +24,32 @@ public class StraightIdentifier implements HandIdentifier {
 	}
 
 	private RankedHand findStraight(String player, Iterable<Card> cards, List<Map.Entry<Integer, Collection<Card>>> cardsByValue) {
-		while (cardsByValue.size() >= 5) {
-			if (cardsByValue.get(0).getKey() - 4 == cardsByValue.get(4).getKey())
-				return new RankedHand(player, cards, this, cardsFrom(cardsByValue));
-			cardsByValue.remove(0);
+		List<Map.Entry<Integer, Collection<Card>>> cardsInStraight = new ArrayList<Map.Entry<Integer, Collection<Card>>>(cardsByValue);
+		while (cardsInStraight.size() >= 5) {
+			if (cardsInStraight.get(0).getKey() - 4 == cardsInStraight.get(4).getKey())
+				return new RankedHand(player, cards, this, cardsFrom(cardsInStraight));
+			cardsInStraight.remove(0);
 		}
 		return null;
 	}
 	
 	private List<Map.Entry<Integer, Collection<Card>>> aceLow(List<Map.Entry<Integer, Collection<Card>>> cardsByValue) {
+		final Collection<Map.Entry<Integer, Collection<Card>>> ace = Iterables.where(cardsByValue, new Predicate<Map.Entry<Integer, Collection<Card>>>() {
+			@Override
+			public boolean evaluate(Entry<Integer, Collection<Card>> item) {
+				return item.getKey() == aceValue;
+			}}); 
+		
+		if (ace.isEmpty()) return cardsByValue;
+
+		cardsByValue.remove(ace.iterator().next());
+		cardsByValue.add(new Map.Entry<Integer, Collection<Card>>() {
+			@Override
+			public Integer getKey() { return 1; }
+			@Override
+			public Collection<Card> getValue() { return ace.iterator().next().getValue(); }
+			@Override
+			public Collection<Card> setValue(Collection<Card> value) { return getValue(); }});
 		return cardsByValue;
 	}
 	
